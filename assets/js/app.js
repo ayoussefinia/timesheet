36250a0c6f1c92cbeee1ef080ff951b622169e0e
ayoussefinia <alex.youssefinia@gmail.com>
2019-06-12 11:33:04 -0500

    skeleton created

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+console.log("connected");
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB8h2LtNcYxgRabGCZgAunkFMBU7MNGhis",
+  authDomain: "june-project-f5e77.firebaseapp.com",
+  databaseURL: "https://june-project-f5e77.firebaseio.com",
+  projectId: "june-project-f5e77",
+  storageBucket: "june-project-f5e77.appspot.com",
+  messagingSenderId: "602603736335",
+  appId: "1:602603736335:web:8f4ecef29152dbd6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
