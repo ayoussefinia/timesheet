b650ae4c4640a9d4578634dba138855b6680c466
Andrew Griswold <andrewgriswold7@gmail.com>
2019-06-13 10:08:07 -0500

    I think these changes are the same as alex's

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,31 +37,32 @@ $("#submit").on("click", function(event) {
    dateAdded: firebase.database.ServerValue.TIMESTAMP
  });
 
-//  Calculates Months Worked / Total Billed.
-var currentDay = function(sp){
-  today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth()+1;
-  var yyyy = today.getFullYear();
+ 
+// //  Calculates Months Worked / Total Billed.
+// var currentDay = function(sp){
+//   today = new Date();
+//   var dd = today.getDate();
+//   var mm = today.getMonth()+1;
+//   var yyyy = today.getFullYear();
 
-  if(dd<10) dd='0'+dd;
-  if(mm<10) mm='0'+mm;
-  return (mm+sp+dd+sp+yyyy);
-  monthsWorked(dd,mm,yyyy);
-  };
-  console.log(currentDay('/'));
+//   if(dd<10) dd='0'+dd;
+//   if(mm<10) mm='0'+mm;
+//   return (mm+sp+dd+sp+yyyy);
+//   monthsWorked(dd,mm,yyyy);
+//   };
+//   console.log(currentDay('/'));
 
-var mm = startDate.slice(0,2);
-var dd = startDate.slice(3,5);
-var yyyy = startDate.slice(6,8);
-console.log(mm);
-console.log(dd);
-console.log(yyyy);
+// var mm = startDate.slice(0,2);
+// var dd = startDate.slice(3,5);
+// var yyyy = startDate.slice(6,8);
+// console.log(mm);
+// console.log(dd);
+// console.log(yyyy);
 
-var monthsWorked = function(curD, curM, curY) {
-  var mWork = curD - mm;
-}
-console.log(monthsWorked(mWork));
+// var monthsWorked = function(curD, curM, curY) {
+//   var mWork = curD - mm;
+// }
+// console.log(monthsWorked(mWork));
 
 
 
